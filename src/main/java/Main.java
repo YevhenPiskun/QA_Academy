42c48842a314f6991be92d0590161344ce2edfe1
Yevhen Piskun <piskuntech@gmail.com>
2018-02-02 18:31:31 +0200

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Цена одного килограма конфет: ");
+        String konfet = reader.readLine();
+        System.out.println("Цена одного килограма печенья: ");
+        String pechen = reader.readLine();
+        double konfetInt = Double.parseDouble(konfet);
+        double pechenInt = Double.parseDouble(pechen);
+        double pokupka1 = 0.3 * konfetInt + 0.4 * pechenInt;
+        double pokupka2 = (2 * pechenInt + 1.8 * konfetInt) * 3;
+        System.out.println("Покупка 1: " + pokupka1);
+        System.out.println("Покупка 2: " + pokupka2);
+
+    }
+}
---
+++ src/main/java/Main2.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите время в минутах: ");
+        String minutesLine = reader.readLine();
+        System.out.println("Введите расстояние в км: ");
+        String rastoyanieLine = reader.readLine();
+        Double minutesDouble = Double.parseDouble(minutesLine);
+        Double rasstoyanieDouble = Double.parseDouble(rastoyanieLine);
+        double skorost = rasstoyanieDouble/(minutesDouble/60);
+        System.out.println("Скорость: " + skorost);
+
+    }
+}
