1b69533cf471d58c5e1e403f0d01efad71a1163f
Yevhen Piskun <piskuntech@gmail.com>
2018-02-12 23:54:40 +0200

    Password generator add comments

--- src/main/java/PasswordGenerator.java
+++ src/main/java/PasswordGenerator.java
@@ -33,25 +33,25 @@ public class PasswordGenerator {
             }
         }
 
-        String[] array = {LETTERS, NUMBER, SYMBOL};
+        String[] array = {LETTERS, NUMBER, SYMBOL};  //массив наборов символов
         int bound = 3;
         int countOfNumbers = 0;
         Random random = new Random();
         String password = "";
         String previousSymbol = "";
         while (password.length() != passwordLength) {
-            int setOfSymbols = random.nextInt(bound);
+            int setOfSymbols = random.nextInt(bound);   //случайный выбор набора символов
             String nextSymbol = returnRandomSymbolFromArray(array[setOfSymbols]);
-            if (nextSymbol.equals(previousSymbol)) {
+            if (nextSymbol.equals(previousSymbol)) {     //проверка на совпадение текущего символа с предыдущим
                 continue;
             } else {
-                if (setOfSymbols == 2) {
+                if (setOfSymbols == 2) {     //проверка на то, что выпал символ из 2-го набора символов
                     password = password.concat(nextSymbol);
                     bound = 2;
                 } else {
-                    if (setOfSymbols == 1) {
+                    if (setOfSymbols == 1) {   //проверка на то, что выпал символ из 1-го набора символов
                         countOfNumbers++;
-                        if (countOfNumbers == 3) {
+                        if (countOfNumbers == 3) {   //проверка на то, что символ из 1-го набора уже выпал 3 раза
                             bound = 1;
                         }
                     }
@@ -66,7 +66,7 @@ public class PasswordGenerator {
 
     }
 
-    private static String returnRandomSymbolFromArray(String line) {
+    private static String returnRandomSymbolFromArray(String line) {      //метод возвращает случайный символ из строки
         Random random = new Random();
         int choice = random.nextInt(line.length());
         return line.substring(choice, choice + 1);
