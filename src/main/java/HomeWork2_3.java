e1f3cfff591206335f7f4384372c56ca3ae14865
Yevhen Piskun <piskuntech@gmail.com>
2018-02-12 22:20:15 +0200

    HW 2, task 3

--- src/main/java/HomeWork2_3.java
+++ src/main/java/HomeWork2_3.java
@@ -1,6 +1,17 @@
 public class HomeWork2_3 {
 
     public static void main(String[] args) {
+        int index = 8;
+        System.out.println(fibo(index));
+    }
 
+    private static int fibo(int count) {
+        int result;
+        if (count == 1 || count == 2) {
+            return 1;
+        } else {
+            result = fibo(count - 1) + fibo(count - 2);
+            return result;
+        }
     }
 }
