118ca222e9a13aa649f43c896b98109ee3f045f9
wolfchkov <wolfchkovaj@gmail.com>
2018-02-13 22:18:54 +0200

    Update PasswordGenerator.java
    Не совсем верное решение.
    Замечания.

--- src/main/java/PasswordGenerator.java
+++ src/main/java/PasswordGenerator.java
@@ -1,8 +1,12 @@
+//Пакет!!!
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Random;
 
+/**
+Всегда при генерации пароль начинается с какого либо символа из набора SYMBOL!
+**/
 public class PasswordGenerator {
 
     private static final int LOW_BOUND = 8;
@@ -37,7 +41,9 @@ public class PasswordGenerator {
         int bound = 3;
         int countOfNumbers = 0;
         Random random = new Random();
+        //лучше использовать StringBuilder
         String password = "";
+        //используй char а не String для одного символа
         String previousSymbol = "";
         while (password.length() != passwordLength) {
             int setOfSymbols = random.nextInt(bound);   //случайный выбор набора символов
@@ -45,16 +51,20 @@ public class PasswordGenerator {
             if (nextSymbol.equals(previousSymbol)) {     //проверка на совпадение текущего символа с предыдущим
                 continue;
             } else {
+                //Магическое число 2
                 if (setOfSymbols == 2) {     //проверка на то, что выпал символ из 2-го набора символов
+                    //дорогая операция! StringBuilder нужен
                     password = password.concat(nextSymbol);
                     bound = 2;
                 } else {
+                    //Магическое число 1
                     if (setOfSymbols == 1) {   //проверка на то, что выпал символ из 1-го набора символов
                         countOfNumbers++;
                         if (countOfNumbers == 3) {   //проверка на то, что символ из 1-го набора уже выпал 3 раза
                             bound = 1;
                         }
                     }
+                    //дорогая операция! StringBuilder нужен
                     password = password.concat(nextSymbol);
                     previousSymbol = nextSymbol;
                 }
@@ -67,8 +77,11 @@ public class PasswordGenerator {
     }
 
     private static String returnRandomSymbolFromArray(String line) {      //метод возвращает случайный символ из строки
+        //Не нужно каждый раз создавать Random, не оптимально!
+        //Создай его один раз как статическое поля класса.
         Random random = new Random();
         int choice = random.nextInt(line.length());
+        //charAt будет лечше
         return line.substring(choice, choice + 1);
     }
 
