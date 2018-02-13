f3253e90248db4e483d7b3e92b1d670d351e870c
wolfchkov <wolfchkovaj@gmail.com>
2018-02-13 22:05:58 +0200

    Update HomeWork3_2.java
    Отлично!
    
    Мелкие замечания

--- src/main/java/HomeWork3_2.java
+++ src/main/java/HomeWork3_2.java
@@ -1,15 +1,19 @@
+//Не задан пакет!!!
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class HomeWork3_2 {
     public static void main(String[] args) throws IOException {
+        //Не ясно пользователю, что от него хотят :)
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String line = reader.readLine();
         char arrSymbolsOfLine[] = line.toCharArray();
         int[] arrSymbols = new int[256];
         for (int i = 0; i < arrSymbolsOfLine.length; i++) {
             arrSymbols[(int)arrSymbolsOfLine[i]] = arrSymbols[(int)arrSymbolsOfLine[i]] + 1;
+            // можно упростить:
+            // arrSymbols[(int)arrSymbolsOfLine[i]]++;
         }
         for (int j = 0; j < arrSymbols.length; j++) {
             if (arrSymbols[j] != 0) {
