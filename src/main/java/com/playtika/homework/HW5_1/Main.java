07cc122ea1490f391f3330729f438c718b4c02ce
Yevhen Piskun <piskuntech@gmail.com>
2018-02-26 23:06:02 +0200

    HW 5

--- src/main/java/com/playtika/homework/HW5_1/Main.java
+++ src/main/java/com/playtika/homework/HW5_1/Main.java
@@ -7,37 +7,33 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Введите первое число: ");
-        double a = Double.parseDouble(reader.readLine());
-        System.out.println("Введите второе число: ");
-        double b = Double.parseDouble(reader.readLine());
-        System.out.println("Введите операцию: ");
-        String operation = reader.readLine();
+        System.out.println("Введите выражение через пробел: ");
+        String line = reader.readLine();
+        String[] arr = line.split(" ");
+        double a = 0;
+        double b = 0;
+        String operation = null;
+        if (arr.length == 3) {
+            a = Double.parseDouble(arr[0]);
+            b = Double.parseDouble(arr[2]);
+            operation = arr[1];
+        }
         System.out.println("Результат: ");
 
-        Calculate calc = new Calculate();
-
         if (operation.equals("+")) {
-            calc.setOperation(new Addition());
-            calc.operation(a, b);
+            new Addition().calculate(a, b);
         } else if (operation.equals("-")) {
-            calc.setOperation(new Subtraction());
-            calc.operation(a, b);
+            new Subtraction().calculate(a, b);
         } else if (operation.equals("*")) {
-            calc.setOperation(new Multiplication());
-            calc.operation(a, b);
+            new Multiplication().calculate(a, b);
         } else if (operation.equals("/")) {
-            calc.setOperation(new Division());
-            calc.operation(a, b);
+            new Division().calculate(a, b);
         } else if (operation.equals("^")) {
-            calc.setOperation(new Degree());
-            calc.operation(a, b);
+            new Degree().calculate(a, b);
         } else if (operation.equals("log")) {
-            calc.setOperation(new Logarithm());
-            calc.operation(a, b);
+            new Logarithm().calculate(a, b);
         } else if (operation.equals("sqrt")) {
-            calc.setOperation(new Root());
-            calc.operation(a, b);
+            new Root().calculate(a, b);
         } else {
             System.out.println("Введена не поддерживаемая операция");
         }
