e7d77211541e40bfa403db83ab4d66a8f19db08c
Yevhen Piskun <piskuntech@gmail.com>
2018-03-04 22:25:42 +0200

    HW 7

--- src/main/java/com/playtika/homework/com/homework7/Main.java
+++ src/main/java/com/playtika/homework/com/homework7/Main.java
@@ -1,6 +1,5 @@
 package com.playtika.homework.com.homework7;
 
-import javax.xml.bind.SchemaOutputResolver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
