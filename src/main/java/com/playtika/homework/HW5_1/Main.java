cc2153ea759942381b8dacf8a7759509a21b1e89
wolfchkov <wolfchkovaj@gmail.com>
2018-03-04 22:59:22 +0200

    Update Main.java

--- src/main/java/com/playtika/homework/HW5_1/Main.java
+++ src/main/java/com/playtika/homework/HW5_1/Main.java
@@ -5,6 +5,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    /**
+    В этом методе нужно обрабатывать исключения WrongOperationException и WrongEnteringException и
+    в catch выводить в консоль соответсвующую ошибку. Программа не должна падать по стектрейсу.
+    
+    Порождаться же исключения должны в отдельном классе, к примеру Calculator, который принимает параметры,
+    строку оператора и выводить результат.
+    
+    **/
     public static void main(String[] args) throws IOException, WrongOperationException, WrongEnteringException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Введите выражение через пробел: ");
@@ -21,18 +29,25 @@ public class Main {
         System.out.println("Результат: ");
         try {
             if (operation.equals("+")) {
+                //зачем нам интерфейс, если ты вызваешь метод калькуляции через конкретный класс????
                 new Addition().calculate(a, b);
             } else if (operation.equals("-")) {
+                //зачем нам интерфейс, если ты вызваешь метод калькуляции через конкретный класс????
                 new Subtraction().calculate(a, b);
             } else if (operation.equals("*")) {
+                //зачем нам интерфейс, если ты вызваешь метод калькуляции через конкретный класс????
                 new Multiplication().calculate(a, b);
             } else if (operation.equals("/")) {
+                //зачем нам интерфейс, если ты вызваешь метод калькуляции через конкретный класс????
                 new Division().calculate(a, b);
             } else if (operation.equals("^")) {
+                //зачем нам интерфейс, если ты вызваешь метод калькуляции через конкретный класс????
                 new Degree().calculate(a, b);
             } else if (operation.equals("log")) {
+                //зачем нам интерфейс, если ты вызваешь метод калькуляции через конкретный класс????
                 new Logarithm().calculate(a, b);
             } else if (operation.equals("sqrt")) {
+                //зачем нам интерфейс, если ты вызваешь метод калькуляции через конкретный класс????
                 new Root().calculate(a, b);
             } else {
                 throw new WrongOperationException("You enter wrong operation");
