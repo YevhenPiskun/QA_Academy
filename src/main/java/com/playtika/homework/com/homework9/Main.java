3ae94481343bdff3a6b4782d2e20d89a994e2e06
Yevhen Piskun <piskuntech@gmail.com>
2018-03-25 22:54:32 +0300

    HW9

--- src/main/java/com/playtika/homework/com/homework9/Main.java
+++ src/main/java/com/playtika/homework/com/homework9/Main.java
@@ -1,11 +1,14 @@
 package com.playtika.homework.com.homework9;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         ReadingFromFile readingFromFile = new ReadingFromFile();
         StreamMethods sm = new StreamMethods();
-        sm.getCountOfFemale(readingFromFile.createListOfEmployees(readingFromFile.readFromFile()));
+        List<Employee> list = readingFromFile.createListOfEmployees(readingFromFile.readFromFile());
+        sm.getCountOfFemale(list);
+        sm.getCountOfMenWithSalaryMoreThen10000(list);
     }
 }
--- src/main/java/com/playtika/homework/com/homework9/StreamMethods.java
+++ src/main/java/com/playtika/homework/com/homework9/StreamMethods.java
@@ -6,10 +6,17 @@ public class StreamMethods {
 
     public void getCountOfFemale(List<Employee> list) {
         System.out.println("Количество женщин: ");
-        System.out.println(list.stream().
-                filter(employee -> employee.
-                        getSex() == Sex.FEMALE).
-                count());
+        System.out.println(list.stream()
+                .filter(employee -> employee.getSex() == Sex.FEMALE)
+                .count());
+    }
+
+    public void getCountOfMenWithSalaryMoreThen10000(List<Employee> list){
+        System.out.println("Количество мужчин с зарплатой более 10000: ");
+        System.out.println(list.stream()
+                .filter(employee -> employee.getSex() == Sex.MALE)
+                .filter(employee -> employee.getSalary() > 10000)
+                .count());
     }
 
 }
